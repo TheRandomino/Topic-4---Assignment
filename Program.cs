16798852ee0429e24fe628c80b3ca87272f90b87
362688756 <362688756@2260I-L29-009>
2026-09-18 12:19:13 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Topic_4___Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //I'm Travis I'm Travis
+
+            Part1();
+
+            //Part2();
+
+            //Part3();
+
+            //Part4();
+        }
+
+        public static void Part1()
+        {
+            Console.WriteLine("Your name is...?");
+            Console.ReadLine();
+        }
+
+        public static void Part2()
+        {
+
+        }
+
+        public static void Part3()
+        {
+
+        }
+
+        public static void Part4()
+        {
+
+        }
+
+
+       
+
+        
+    }
+}
