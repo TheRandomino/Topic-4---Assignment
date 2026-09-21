9ffb740519ae198fa0bd001202f2afc1236f7537
362688756 <362688756@2260I-L29-009>
2026-09-21 13:13:19 -0400

    done easy peasy lemon squeezy

--- Program.cs
+++ Program.cs
@@ -9,13 +9,15 @@ namespace Topic_4___Assignment
 
             //I'm Travis I'm Travis
 
-            //Part1();
+            Part1();
 
-            //Part2();
+            Part2();
 
             Part3();
 
-            //Part4();
+            Part4();
+
+            Part5();
         }
 
         public static void Part1()
@@ -34,6 +36,8 @@ namespace Topic_4___Assignment
             Console.WriteLine(Double.TryParse(Console.ReadLine(), out salary));
             Console.WriteLine("");
             Console.WriteLine("You make " + salary.ToString("C") + " an hour? That's more than me!");
+            Console.WriteLine("");
+            Console.WriteLine("");
         }
 
         public static void Part2()
@@ -62,7 +66,8 @@ namespace Topic_4___Assignment
             Console.WriteLine("ID: \t\t" + studentID);
             Console.WriteLine("Login: \t\t" + login);
             Console.WriteLine("Average Mark: \t" + studentAvgMark);
-
+            Console.WriteLine("");
+            Console.WriteLine("");
         }
 
         public static void Part3()
@@ -77,17 +82,65 @@ namespace Topic_4___Assignment
             Console.WriteLine(Int32.TryParse(Console.ReadLine(), out age));
             Console.WriteLine("");
             Console.WriteLine(age + ", hm? Well that ain't too bad! Did ya know that you were " + (age - 5) + " years old five years ago, and you'll be " + (age + 5) + " years old in five years! Well... given you don't die of course.");
-
+            Console.WriteLine("");
+            Console.WriteLine("");
         }
 
         public static void Part4()
         {
+            double numberOne = 0, numberTwo = 0, numberThree = 0;
+            Console.Write("Give me a number! ");
+            Console.WriteLine(Double.TryParse(Console.ReadLine(), out numberOne));
+            Console.Write("Give me a number again! ");
+            Console.WriteLine(Double.TryParse(Console.ReadLine(), out numberTwo));
+            Console.Write("Give me a number one more time! ");
+            Console.WriteLine(Double.TryParse(Console.ReadLine(), out numberThree));
+            Console.WriteLine("Your super duper secret number is " + (numberOne + numberTwo + numberThree)/2 + "!");
+            Console.WriteLine("");
+            Console.WriteLine("");
+        }
+
+        public static void Part5()
+        {
+            string item1 = "", item2 = "";
+            double item1Price = 0, item2Price = 0, flatTotal = 0, discountCut = 0, subTotal = 0, taxAdd = 0, total = 0;
+            const double discount = .2, tax = .13;
+
+            Console.Write("Item 1: ");
+            item1 = Console.ReadLine();
+            Console.Write("Item 1 Price: ");
+            Console.WriteLine(Double.TryParse(Console.ReadLine(), out item1Price));
+            Console.Write("Item 2 : ");
+            item2 = Console.ReadLine();
+            Console.Write("Item 2 Price: ");
+            Console.WriteLine(Double.TryParse(Console.ReadLine(), out item2Price));
+            Console.WriteLine("");
+
+            flatTotal = item1Price + item2Price;
+            discountCut = flatTotal * discount;
+            subTotal = flatTotal - discountCut;
+            taxAdd = subTotal * tax;
+            total = subTotal + taxAdd;
+
+            Console.WriteLine("Your Receipt:");
+            Console.WriteLine("");
+            Console.WriteLine("Item 1: " + item1);
+            Console.WriteLine("Price: " + item1Price.ToString("C"));
+            Console.WriteLine("Item 2:" + item2);
+            Console.WriteLine("Price: " + item2Price.ToString("C"));
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+            Console.WriteLine("Flat Total: " + flatTotal.ToString("C"));
+            Console.WriteLine("Discount (20%): " + discountCut.ToString("C"));
+            Console.WriteLine("Subtotal: " + subTotal.ToString("C"));
+            Console.WriteLine("Tax (13%): " + taxAdd.ToString("C"));
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+            Console.WriteLine("Total: " + total.ToString("C"));
+
 
         }
 
 
-       
 
-        
+
     }
 }
